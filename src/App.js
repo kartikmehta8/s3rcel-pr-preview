cd1f1d411dc2470a00a47342000a4e35bfcae8de
Kartik Mehta <77505989+kartikmehta8@users.noreply.github.com>
2025-03-18 12:12:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import s3Logo from './assets/s3.svg';
-import cloudfrontLogo from './assets/cloudfront.png';
-
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-8">
@@ -17,17 +14,6 @@ function App() {
         <div className="bg-white shadow overflow-hidden rounded-lg">
           <div className="px-4 py-5 sm:p-6">
             <div className="flex justify-center space-x-8 mb-8">
-              {/* AWS S3 Logo */}
-              <div className="flex flex-col items-center">
-                <img src={s3Logo} alt="Amazon S3" className="w-16 h-16 object-contain" />
-                <span className="mt-2 text-sm font-medium text-gray-900">Amazon S3</span>
-              </div>
-
-              {/* CloudFront Logo */}
-              <div className="flex flex-col items-center">
-                <img src={cloudfrontLogo} alt="CloudFront" className="w-16 h-16 object-contain" />
-                <span className="mt-2 text-sm font-medium text-gray-900">CloudFront</span>
-              </div>
             </div>
 
             <div className="prose prose-indigo mx-auto">
